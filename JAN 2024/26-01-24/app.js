d384c2af13387cf92f2ce4fc6eca74f4d4588828
Azeez Roheem <mediatoor@gmail.com>
2024-01-26 20:23:10 +0000

    2024-01-26

---
+++ JAN 2024/26-01-24/app.js
@@ -0,0 +1,52 @@
+/** Task:
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+```
+
+Hint:  ||HASH MAP PATTERN!||
+
+From: https://leetcode.com/problems/jewels-and-stones/
+
+Today's Solution: 
+https://replit.com/@leonanoel/count-chars-huntober2022
+
+<a:pikahello:809296783448342548>   See you at standup!
+
+Answer:
+*/
+
+function findJewels(jewels,stones){
+    const map = {}
+    let count = 0
+  
+    for(const jewel of jewels){
+      map[jewel] = true
+    }
+  
+    for(const stone of stones){
+      if( map[stone] ){
+        count++
+      }
+    }
+    return count
+  }
+  
+  console.log(findJewels("aA","aAAbbbb"), "3")
+  
