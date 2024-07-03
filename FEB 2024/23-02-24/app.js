107c7ddc42bc67b3350eae16945c13bd63e11af7
Azeez Roheem <mediatoor@gmail.com>
2024-07-03 16:42:11 +0100

    2024-07-03

---
+++ FEB 2024/23-02-24/app.js
@@ -0,0 +1,13 @@
+/* Task:
+
+Implement a function which convert the given boolean value into its string representation.
+
+Answer:
+*/
+
+function booleanToString(bool) {
+    return bool.toString();
+  }
+  
+  console.log(booleanToString(true))
+  console.log(booleanToString(false))
