f02b226753c0feb04883838a1f02edf6494210b0
Azeez Roheem <mediatoor@gmail.com>
2024-08-27 19:35:42 +0100

    2024-07-27

---
+++ AUG 2024/27-08-24/app.js
@@ -0,0 +1,11 @@
+/* Task:
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+Answer:
+*/
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    // TODO
+    return distanceToPump <= mpg * fuelLeft
+  };
