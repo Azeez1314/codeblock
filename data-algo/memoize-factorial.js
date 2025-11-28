8c3a598679c81bfe4a28b64a2bb5dd46b716d4fa
John Doe <mediatoor@gmail.com>
2025-11-28 14:09:47 +0000

    memoize update

--- data-algo/memoize-factorial.js
+++ data-algo/memoize-factorial.js
@@ -40,6 +40,26 @@ try {
 
 
 
+// const times10 = (n) => (n * 10);
+// Task 4: Make your memo function generic and accept the times10 function as a 
+// callback rather than defining the n * 10 logic inside the if/she or pulling it 
+// in from the global scope.
+
+// protip: Take advantage of the fact that parameters are saved in the closure as 
+// well, just like the cache from the previous example.
+const memoize = (cb) => {
+
+}
+
+// returned function from memoizedAdd
+const memoizedTimes10 = memoize(times10);
+console.log('~~~~~~~~~~~~~~~~~~~~TASK 4~~~~~~~~~~~~~~~~~~~~');
+try {
+    console.log('Task 4 calculated value:', memoizedTimes10(9)); // calculated
+    console.log('Task 4 cached value:', memoizedTimes10(9)); // cached
+} catch (error) {
+    console.error('Task 4:', error)
+}
 
 
 
@@ -117,3 +137,39 @@ console.log('Task 2 cached value:', memoTimes10(9)); // cached
 //     } catch (error) {
 //         console.error('Task 3:', error)
 //     }
+
+
+
+
+// const times10 = (n) => (n * 10);
+// Task 4: Make your memo function generic and accept the times10 function as a 
+// callback rather than defining the n * 10 logic inside the if/she or pulling it 
+// in from the global scope.
+
+// protip: Take advantage of the fact that parameters are saved in the closure as 
+// well, just like the cache from the previous example.
+// const memoize = (cb) => {
+    //     const cache = {}
+
+//     return (n) => {
+//         if (n in cache) {
+//             console.log('Fetching from cache:', n)
+//             return cache[n]
+//         } else {
+//             console.log('Calculating result')
+//             let result = cb(n)
+//             cache[n] = result
+//             return result
+//         }
+//     }
+// }
+
+// returned function from memoizedAdd
+// const memoizedTimes10 = memoize(times10);
+// console.log('~~~~~~~~~~~~~~~~~~~~TASK 4~~~~~~~~~~~~~~~~~~~~');
+// try {
+//     console.log('Task 4 calculated value:', memoizedTimes10(9)); // calculated
+//     console.log('Task 4 cached value:', memoizedTimes10(9)); // cached
+// } catch (error) {
+//     console.error('Task 4:', error)
+// }
---
+++ practice.js
@@ -0,0 +1,13 @@
+
+function findWeekendBirthdays(date_of_birth) {
+    
+}
+
+
+// Example usage:
+const date_of_birth = "23-10";
+console.log(findWeekendBirthdays(date_of_birth));
+
+
+
+
