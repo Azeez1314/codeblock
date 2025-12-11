319c7472c8404e867628d4a5b31a912c98eff7a5
John Doe <mediatoor@gmail.com>
2025-12-11 09:21:08 +0000

    2025-12-11

---
+++ DEC 2025/2025-12-11/app.js
@@ -0,0 +1,11 @@
+// Task:
+// Practice Challenge Example
+
+// Write a function that takes a number and returns whether it’s positive, negative, or zero.
+
+const tasks = ["Do laundry", "Finish homework", "Buy groceries", "Exercise"];
+
+for (let i = 0; i < tasks.length; i++) {
+  console.log(`${i + 1}. ${tasks[i]}`);
+}
+
