3e3066b90de5141f8960485494b67307f6131620
John Doe <mediatoor@gmail.com>
2026-01-27 10:15:06 +0000

    2026-01-27

---
+++ data-algo/2026-01-27/11-greedyAlgorithm.js
@@ -0,0 +1,29 @@
+// Task:
+// Write a function, makeChange, that returns an integer that represents the least number of coins that add up
+// to an amount where the amount is always divisible by 5.
+
+const makeChange = (value) => { 
+    if (value === 0) return 0;
+    let minCoins;
+    coins.forEach(coin => {
+        if (value - coin >= 0) {
+            let currMinCoins = makeChange(value - coin);
+            if(minCoins == undefined || currMinCoins < minCoins) {
+                minCoins = currMinCoins;
+            }
+        }
+    })
+    return minCoins + 1
+}
+
+
+
+// coin values: 5, 10, 25
+
+
+
+
+
+
+
+// input amount: 40, output # of coins: 3 (25, 10, 5)
