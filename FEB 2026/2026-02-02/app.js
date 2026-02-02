fda3b188bd852442cc42d6b76198a725b2088cb4
John Doe <mediatoor@gmail.com>
2026-02-02 17:23:06 +0000

    2026-02-02

---
+++ FEB 2026/2026-02-02/app.js
@@ -0,0 +1,25 @@
+// Task:
+// Fun with lists: length (click to show)
+// Implement the method length, which accepts a linked list (head), and returns the length of the list.
+
+// For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+
+// The linked list is defined as follows:
+
+// function Node(data, next = null) {
+//   this.data = data;
+//   this.next = next;
+// }
+// Note: the list may be null and can hold any type of value.
+
+
+// Answer:
+function length(head) {
+  let len = 0;
+  while (head) {
+    head = head ? head.next : null;
+    len++;
+  }
+
+  return len;
+}
