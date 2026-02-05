3ec584ba7378d9816fcc5903ef569b3b96f0a68b
John Doe <mediatoor@gmail.com>
2026-02-05 11:28:36 +0000

    2026-02-05

---
+++ FEB 2026/2026-02-05/app.js
@@ -0,0 +1,27 @@
+// Task:
+// Largest 5 digit number in a series (click to show)
+// In the following 6 digit number:
+
+// 283910
+// 91 is the greatest sequence of 2 consecutive digits.
+
+// In the following 10 digit number:
+
+// 1234567890
+// 67890 is the greatest sequence of 5 consecutive digits.
+
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the 
+// number given. The number will be passed in as a string of only digits. It should return a five digit integer. 
+// The number passed may be as large as 1000 digits.
+
+// Adapted from ProjectEuler.net
+
+
+//Answer:
+function solution(digits){
+    max = +digits.slice(0, 5);
+    for(let i = 0; i < digits.length; i++) {
+      if(digits.slice(i, i + 5) > max) max = +digits.slice(i, i + 5);
+    }
+    return max;
+  }
