fa5a6c8caf13abcccb366f3fc230971aaac28231
John Doe <mediatoor@gmail.com>
2026-02-06 10:27:16 +0000

    2026-02-06

---
+++ FEB 2026/2026-02-06/app.js
@@ -0,0 +1,13 @@
+// Task:
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an 
+// exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+// Answer:
+var greet = function (n) {
+    return 'Hello ' + n.slice(0, 1).toUpperCase() + n.slice(1).toLowerCase() + '!';
+  };
