f37fcc069ea462f92a63a1b9be8ed6ae19f3d4ae
John Doe <mediatoor@gmail.com>
2026-02-09 16:24:50 +0000

    2026-02-08

---
+++ FEB 2026/2026-02-09/app.js
@@ -0,0 +1,22 @@
+// Task:
+// Given a sequence of numbers, find the largest pair sum in the sequence.
+
+// For example
+
+// [10, 14, 2, 23, 19] --> 42 (i.e. sum of 23 and 19)
+// [99, 2, 2, 23, 19]  --> 122 (i.e. sum of 99 and 23)
+// Input sequence contains minimum two elements and every element is an integer.
+
+
+// Answer:
+function largestPairSum(numbers)
+{
+  let max = numbers[0] + numbers[1];
+  for(let i = 0; i < numbers.length; i++) {
+    for(let j = i + 1; j < numbers.length; j++) {
+      if(numbers[i] + numbers[j] > max) max = numbers[i] + numbers[j];   
+    }
+  }
+  
+  return max;
+}
